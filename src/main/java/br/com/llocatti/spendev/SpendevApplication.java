7351857c34981716e52322d2c37585714b63efb4
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-11-02 01:04:24 -0300

    First commit

---
+++ src/main/java/br/com/llocatti/spendev/SpendevApplication.java
@@ -0,0 +1,12 @@
+package br.com.llocatti.spendev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendevApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpendevApplication.class, args);
+  }
+}
